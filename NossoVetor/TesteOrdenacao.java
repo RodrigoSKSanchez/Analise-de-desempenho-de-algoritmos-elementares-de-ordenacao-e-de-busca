7fed6f1a16f88321283d7e4f14173c5d76141d7d
GustavoTamura <165435866+GustavoTamura@users.noreply.github.com>
2025-03-27 11:45:52 -0300

    inicio 2

---
+++ NossoVetor/TesteOrdenacao.java
@@ -0,0 +1,53 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class TesteOrdenacao {
+    public static void main(String[] args) {
+        NossoVetor v;
+        Scanner scanner = new Scanner(System.in);
+        int t;
+        System.out.print ("escolha o tamanho do vetor, 0 encerra: ");
+        t = scanner.nextInt();
+        while (t > 0) {
+            v = new NossoVetor(t);
+            v.preencheVetor();
+            //System.out.println("vetor original:\n" + v);
+            long ini = new Date().getTime();
+            v.bubbleSort();
+            long fim = new Date().getTime();
+            System.out.println("\nbubble demorou " + (fim - ini) + " milissegundos");
+            //System.out.println("vetor ordenado pelo buble:\n" + v);
+
+            v.preencheVetor();
+            ini = new Date().getTime();
+            v.selectionSort();
+            fim = new Date().getTime();
+            System.out.println("\nselection demorou " + (fim - ini) + " milissegundos");
+
+            v.preencheVetor();
+            ini = new Date().getTime();
+            v.insertionSort();
+            fim = new Date().getTime();
+            System.out.println("\ninsertion demorou " + (fim - ini) + " milissegundos");
+
+            ini = new Date().getTime();
+            v.bubbleSort();
+            fim = new Date().getTime();
+            System.out.println("\nbubble ordenado demorou " + (fim - ini) + " milissegundos");
+
+            ini = new Date().getTime();
+            v.selectionSort();
+            fim = new Date().getTime();
+            System.out.println("\nselection ordnado demorou " + (fim - ini) + " milissegundos");
+
+            ini = new Date().getTime();
+            v.insertionSort();
+            fim = new Date().getTime();
+            System.out.println("\ninsertion ordenado demorou " + (fim - ini) + " milissegundos");
+
+            System.out.print("\nescolha o novo tamanho, 0 encerra: ");
+            t = scanner.nextInt();
+        }
+        scanner.close();
+    }
+}
