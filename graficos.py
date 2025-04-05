f855558f4f4631f83c248b534b274d90349a24d7
GustavoTamura <165435866+GustavoTamura@users.noreply.github.com>
2025-04-04 21:25:25 -0300

    grafico foda

--- graficos.py
+++ graficos.py
@@ -1,25 +1,35 @@
 import matplotlib.pyplot as plt
-#python -m pip install -U pip
-#python -m pip install -U matplotlib
 
 def criar_grafico():
-    resultados = []
+    tamanhos = ['100k', '200k', '400k', '800k', '1.6M']
+    operacoes = []
+
+    titulo = input("digite o título do gráfico: ")
+
+    print("insira os valores de operações para os seguintes tamanhos (em bilhões - B):")
     
-    for i in range(5):
+    for tamanho in tamanhos:
         while True:
             try:
-                valor = float(input(f"Digite o {i+1}º valor: "))
-                resultados.append(valor)
+                valor = float(input(f"{tamanho}: "))
+                operacoes.append(valor)
                 break
             except ValueError:
-                print("Entrada inválida. Por favor, insira um número válido.")
-    
-    plt.plot(range(1, 6), resultados, marker='o', linestyle='-', color='b', label='Resultados')
-    plt.xlabel("Tentativas")
-    plt.ylabel("Valores")
-    plt.title("Gráfico de Linha dos Resultados")
+                print("valor inválido, tente novamente")
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(tamanhos, operacoes, marker='o', linestyle='-', color='b', label='total de operações')
+
+    for i, valor in enumerate(operacoes):
+        texto = f'{valor:.1f}B'
+        plt.text(tamanhos[i], valor, texto, fontsize=10, ha='center', va='bottom')
+
+    plt.title(titulo.lower())
+    plt.xlabel('tamanho do vetor')
+    plt.ylabel('total de operações (bilhões - B)')
     plt.legend()
     plt.grid(True)
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
